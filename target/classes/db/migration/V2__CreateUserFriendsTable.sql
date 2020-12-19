3dcb6cbdf063897d0ad066a68c08192e1370a394
hwandefan <sokolast15@gmail.com>
2020-12-19 20:11:25 +0100

    Delete target folder

--- target/classes/db/migration/V2__CreateUserFriendsTable.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE "userFriends"(
-    id UUID NOT NULL PRIMARY KEY,
-    idUser UUID NOT NULL REFERENCES "user" (id),
-    idFriend UUID NOT NULL REFERENCES "user" (id)
-);
--- target/classes/db/migration/V3__ChatDbCreate.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "ChatDB"(
-    id UUID NOT NULL PRIMARY KEY,
-    senderId UUID NOT NULL REFERENCES "user" (id),
-    recieverId UUID NOT NULL REFERENCES "user" (id),
-    message VARCHAR (1024) NOT NULL,
-    encodeStep INTEGER NOT NULL
-);
